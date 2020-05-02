5bad94544a4630ae1c07c80106a5048ed46db893
Yingwen <inwen.chen@gmail.com>
2020-05-02 09:22:42 -0600

    changing variable name to camelCased

--- db/index.js
+++ db/index.js
@@ -6,10 +6,10 @@ const db = mysql.createConnection({
   database: 'photo_gallery',
 });
 
-const getMainRouteString = (id) => {
+const getMainRouteString = (listingId) => {
   return new Promise((resolve, reject) => {
-    let select_query_name = `SELECT * FROM Photos WHERE name='${id}'`;
-    db.query(select_query_name, (err, results) => {
+    let queryString = `SELECT * FROM Photos WHERE name='${listingId}'`;
+    db.query(queryString, (err, results) => {
       if (err) {
         reject(err);
       }
@@ -22,8 +22,8 @@ const getMainRouteString = (id) => {
 const insertDataSet = (data) => {
 
   return new Promise((resolve, reject) => {
-    let select_query_name = `INSERT INTO Photos SET ?`;
-    db.query(select_query_name, data, (err, results) => {
+    let queryString = `INSERT INTO Photos SET ?`;
+    db.query(queryString, data, (err, results) => {
       if (err) {
         reject(err);
       }
@@ -34,10 +34,10 @@ const insertDataSet = (data) => {
 
 //Delete set of Data where id is...
 
-const deleteDataSet = (id) => {
+const deleteDataSet = (listingId) => {
   return new Promise ((resolve, reject) => {
-    let select_query_name = `DELETE FROM Photos WHERE ID = ${id}`;
-    db.query(select_query_name, (err, results) => {
+    let queryString = `DELETE FROM Photos WHERE ID = ${listingId}`;
+    db.query(queryString, (err, results) => {
       if (err) {
         reject(err);
       }
@@ -47,10 +47,10 @@ const deleteDataSet = (id) => {
 }
 
 //update data set
-const updateDataSet = (id, item, newData) => {
+const updateDataSet = (listingId, item, newData) => {
   return new Promise((resolve, reject) => {
-    let select_query_num = `UPDATE Photos SET ${item} = ${newData} WHERE listing_id=${id}`;
-    db.query(select_query_num, (err, results) => {
+    let queryString = `UPDATE Photos SET ${item} = ${newData} WHERE listing_id=${listingId}`;
+    db.query(queryString, (err, results) => {
       if (err) {
         reject(err);
       }
@@ -59,10 +59,10 @@ const updateDataSet = (id, item, newData) => {
   })
 }
 
-const getMainRouteNum = (id) => {
+const getMainRouteNum = (listingId) => {
   return new Promise((resolve, reject) => {
-    let select_query_num = `SELECT * FROM Photos WHERE listing_id=${id}`;
-    db.query(select_query_num, (err, results) => {
+    let queryString = `SELECT * FROM Photos WHERE listing_id=${listingId}`;
+    db.query(queryString, (err, results) => {
       if (err) {
         reject(err);
       }
@@ -73,8 +73,8 @@ const getMainRouteNum = (id) => {
 
 const toggleFavorite = (id) => {
   return new Promise((resolve, reject) => {
-    let update_query = `UPDATE Photos SET is_favorite = 1 - is_favorite WHERE listing_id=${id}`;
-    db.query(update_query, (err, results) => {
+    let queryString = `UPDATE Photos SET is_favorite = 1 - is_favorite WHERE listing_id=${id}`;
+    db.query(queryString, (err, results) => {
       if (err) {
         reject(err);
       }
@@ -83,10 +83,10 @@ const toggleFavorite = (id) => {
   });
 };
 
-const recPhotos = (id) => {
+const recPhotos = (listingId) => {
   return new Promise((resolve, reject) => {
-    let select_query = `SELECT photo1_b, photo2_b, photo3_b, photo4_b, photo5_b, photo6_b, photo7_b, photo8_b, photo9_b, photo10_b, photo11_b, photo12_b, photo13_b, photo14_b, photo15_b, photo16_b, photo17_b, photo18_b, photo19_b, photo20_b, photo21_b, photo22_b, photo23_b, photo24_b, photo25_b, photo26_b, photo27_b, photo28_b, photo29_b, photo30_b FROM Photos WHERE listing_id=${id}`;
-    db.query(select_query, (err, results) => {
+    let queryString = `SELECT photo1_b, photo2_b, photo3_b, photo4_b, photo5_b, photo6_b, photo7_b, photo8_b, photo9_b, photo10_b, photo11_b, photo12_b, photo13_b, photo14_b, photo15_b, photo16_b, photo17_b, photo18_b, photo19_b, photo20_b, photo21_b, photo22_b, photo23_b, photo24_b, photo25_b, photo26_b, photo27_b, photo28_b, photo29_b, photo30_b FROM Photos WHERE listing_id=${listingId}`;
+    db.query(queryString, (err, results) => {
       if (err) {
         reject(err);
       }
